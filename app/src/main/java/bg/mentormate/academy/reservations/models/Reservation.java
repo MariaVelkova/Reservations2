641364e21791036ef101b269508e9f505587e2d1
Maria Velkova <welkowa@gmail.com>
2015-02-21 17:47:00 +0200

    Reservation Adapter Fix

--- app/src/main/java/bg/mentormate/academy/reservations/models/Reservation.java
+++ app/src/main/java/bg/mentormate/academy/reservations/models/Reservation.java
@@ -6,25 +6,33 @@ package bg.mentormate.academy.reservations.models;
 public class Reservation {
     private int id;
     private int user_id;
+    private String user_first_name;
+    private String user_last_name;
+    private String user_phone;
     private int venue_id;
     private String venue_name;
     private String venue_phone;
     private String venue_city;
     private String venue_address;
+    private String venue_image;
     private int date_created;
     private int date_booked;
     private int people_count;
     private String comment;
     private int accepted;
 
-    public Reservation(int id, int user_id, int venue_id, String venue_name, String venue_phone, String venue_city, String venue_address, int date_created, int date_booked, int people_count, String comment, int accepted) {
+    public Reservation(int id, int user_id, String user_first_name, String user_last_name, String user_phone, int venue_id, String venue_name, String venue_phone, String venue_city, String venue_address, String venue_image, int date_created, int date_booked, int people_count, String comment, int accepted) {
         this.id = id;
         this.user_id = user_id;
+        this.user_first_name = user_first_name;
+        this.user_last_name = user_last_name;
+        this.user_phone = user_phone;
         this.venue_id = venue_id;
         this.venue_name = venue_name;
         this.venue_phone = venue_phone;
         this.venue_city = venue_city;
         this.venue_address = venue_address;
+        this.venue_image = venue_image;
         this.date_created = date_created;
         this.date_booked = date_booked;
         this.people_count = people_count;
@@ -126,5 +134,37 @@ public class Reservation {
 
     public void setVenue_address(String venue_address) {
         this.venue_address = venue_address;
+    }
+
+    public String getUser_first_name() {
+        return user_first_name;
+    }
+
+    public void setUser_first_name(String user_first_name) {
+        this.user_first_name = user_first_name;
+    }
+
+    public String getUser_last_name() {
+        return user_last_name;
+    }
+
+    public void setUser_last_name(String user_last_name) {
+        this.user_last_name = user_last_name;
+    }
+
+    public String getUser_phone() {
+        return user_phone;
+    }
+
+    public void setUser_phone(String user_phone) {
+        this.user_phone = user_phone;
+    }
+
+    public String getVenue_image() {
+        return venue_image;
+    }
+
+    public void setVenue_image(String venue_image) {
+        this.venue_image = venue_image;
     }
 }
